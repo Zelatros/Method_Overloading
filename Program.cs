6afdbdb4769c17eeca360b44acafad5dbd4669f1
T.Tuna <t.tunakoseler@gmail.com>
2022-12-24 14:25:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // out parameter
+        string sayi = "999";
+
+        bool sonuc = int.TryParse(sayi,out int outSayi);
+        if(sonuc)
+        {
+            Console.WriteLine("Başarılı");
+            Console.WriteLine(outSayi);
+        }
+        else
+        {
+            Console.WriteLine("başarısız!");
+        }
+
+        Methods instance = new Methods();
+        instance.Topla(2,3, out int toplamSonuc);
+        Console.WriteLine(toplamSonuc);
+
+        //Method Overloading (Aşırı yükleme)
+        int ifade = 999;
+        instance.EkranaYazdir(Convert.ToString(ifade));
+        instance.EkranaYazdir(ifade);
+        instance.EkranaYazdir("Tuna", "Köseler");
+    }
+}
+
+class Methods
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+     public void EkranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1 + veri2);
+    }
+}
